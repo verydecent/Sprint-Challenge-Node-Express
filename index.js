6a98cf61f750de071346514e84b428b30a7c92d4
verydecent <wnjhwng@gmail.com>
2018-10-12 09:38:21 -0700

    adds get performance

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const server = express();
+const actionMod = require('./data/helpers/actionModel');
+const projectMod = require('./data/helpers/projectModel');
 
 server.use(express.json());
 
@@ -9,6 +11,16 @@ server.get('/', (req, res) => {
 })
 
 //// get ////
+server.get('/list', (req, res) => {
+    actionMod
+        .get()
+            .then(projects => (
+                res.json(projects)
+            ))
+            .catch(err => (
+                res.status(500).json({ error: "Resources could not be found" })
+            ))
+})
 
 //// get id ////
 
